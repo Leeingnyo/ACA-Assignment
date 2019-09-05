3b08a0df1c965747252636ac86cbf28ef28af653
Leeingnyo <dyd1928@naver.com>
2019-09-05 17:06:44 +0900

    make Screen, Camera, implement glfw callback

--- main.cpp
+++ main.cpp
@@ -7,10 +7,145 @@
 #include "glm/gtc/quaternion.hpp"
 #include "glm/gtx/quaternion.hpp"
 
-int main () {
-    printf("Hello, World!");
+class Camera {
+private:
+    static constexpr float MAX_FOV = 135.f;
+    static constexpr float MIN_FOV = 0.0f;
 
+    glm::vec3 eye = {0, 0, 10};
+    glm::vec3 ori = {0, 0, 0}; // at
+    glm::vec3 up = {0, 1, 0};
+    float fov = 90.f;
+    float near = 0.1f;
+    float far = 100.f;
+public:
+    Camera() {
+    }
+
+    void setEye(const glm::vec3& eye) {
+        this->eye = eye;
+    }
+    void setUp(const glm::vec3& up) {
+        this->up = up;
+    }
+
+    const glm::vec3& getEye() const {
+        return eye;
+    }
+    const glm::vec3& getOrigin() const {
+        return ori;
+    }
+    const glm::vec3& getUp() const {
+        return up;
+    }
+    float getFov() const {
+        return fov;
+    }
+    auto getNear() const {
+        return near;
+    }
+    auto getFar() const {
+        return far;
+    }
+};
+
+class Screen {
+private:
+    Camera camera;
+    int width;
+    int height;
+
+    bool is_mouse_left_clicked = true;
+public:
+    static Screen* current_screen;
+
+    Screen() : camera() {
+    }
+
+    void setCurrentScreen() {
+        current_screen = this;
+    }
+
+    const Camera& getCamera() const {
+        return camera;
+    }
+
+    float getAspect() const {
+        return width / (float) height;
+    }
+
+    int getWidth() const {
+        return width;
+    }
+    int getHeight() const {
+        return height;
+    }
+
+    int* getWidthPointer() {
+        return &width;
+    }
+    int* getHeightPointer() {
+        return &height;
+    }
+
+    void cursor_position_callback(GLFWwindow* window, double xpos, double ypos) {
+        static double prev_x = 0;
+        static double prev_y = 0;
+        static double prev_z = 0;
+
+        if (!is_mouse_left_clicked){
+            return;
+        }
+
+        static glm::quat q(1, 0, 0, 0);
+
+        double x = (xpos - width / 2) / (width / 2);
+        double y = -(ypos - height / 2) / (height / 2);
+        double r = sqrt(x * x + y * y);
+        double z = r >= 1 ? 0 : sqrt(1 - r * r);
+
+        if (!(prev_x == 0 && prev_y == 0)){
+            glm::vec3 now(x, y, z);
+            glm::vec3 prev(prev_x, prev_y, prev_z);
+
+            now = q * now;
+            prev = q * prev;
+
+            glm::vec3 axis = glm::normalize(glm::cross(now, prev));
+
+            float costheta = glm::dot(now, prev) / (glm::length(now) * glm::length(prev));
+            float cos = sqrt((costheta + 1) / 2.0f);
+            float sin = sqrt((1 - costheta) / 2.0f);
+
+            if (sin < 0.002f){
+                sin = 0;
+                cos = 1;
+            }
+
+            glm::quat q2 = glm::quat(cos, sin * axis.x, sin * axis.y, sin * axis.z);
+            glm::vec3 v = camera.getEye() - camera.getOrigin();
+            v = q2 * v;
+            camera.setEye(v + camera.getOrigin());
+            camera.setUp(q2 * camera.getUp());
+            q = q2 * q;
+        }
+
+        prev_x = x;
+        prev_y = y;
+        prev_z = z;
+    }
+};
+
+Screen* Screen::current_screen;
+
+void cursor_position_callback(GLFWwindow* window, double xpos, double ypos) {
+    Screen::current_screen->cursor_position_callback(window, xpos, ypos);
+}
+
+int main () {
     GLFWwindow* window;
+    Screen screen = Screen();
+    screen.setCurrentScreen();
 
     // Initialize the library
     if (!glfwInit()) {
@@ -27,7 +162,7 @@ int main () {
     // Make the window's context current
     glfwMakeContextCurrent(window);
     // glfwSetKeyCallback(window, key_callback);
-    // glfwSetCursorPosCallback(window, cursor_position_callback);
+    glfwSetCursorPosCallback(window, cursor_position_callback);
     // glfwSetScrollCallback(window, scroll_callback);
     // glfwSetMouseButtonCallback(window, mouse_button_callback);
     glfwSwapInterval(1);
@@ -42,27 +177,16 @@ int main () {
     // glEnable(GL_LIGHT0);
     // glEnable(GL_COLOR_MATERIAL);
 
-    // Screen
-    int width, height;
-
-    // Camera
-    glm::vec3 eye(0, 0, 10.0f);
-    glm::vec3 ori(0, 0, 0);
-    glm::vec3 up(0, 1, 0);
-    float fov = 90.f;
-
     while (!glfwWindowShouldClose(window)) {
         // Render here
-        float ratio;
-        glfwGetFramebufferSize(window, &width, &height);
-        ratio = width / (float) height;
-        glViewport(0, 0, width, height);
-        // glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+        glfwGetFramebufferSize(window, screen.getWidthPointer(), screen.getHeightPointer());
+        glViewport(0, 0, screen.getWidth(), screen.getHeight());
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
         { // projection
             glMatrixMode(GL_PROJECTION);
             glLoadIdentity();
-            gluPerspective(fov, ratio, 0.1f, 100.0f);
+            gluPerspective(screen.getCamera().getFov(), screen.getAspect(), 0.1f, 100.0f);
         }
 
         { // display
@@ -78,6 +202,9 @@ int main () {
             glLightfv(GL_LIGHT0, GL_POSITION, position);
             */
 
+            const glm::vec3& eye = screen.getCamera().getEye();
+            const glm::vec3& ori = screen.getCamera().getOrigin();
+            const glm::vec3& up = screen.getCamera().getUp();
     	    gluLookAt(eye.x, eye.y, eye.z, ori.x, ori.y, ori.z, up.x, up.y, up.z);
 
             // glTranslatef(0, -0.5, 0);
