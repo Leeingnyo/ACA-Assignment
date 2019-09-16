aaf07fcb26954d89226cb3431d3a52e1a26aeb2a
Leeingnyo <dyd1928@naver.com>
2019-09-16 12:46:37 +0900

    replace kinematics open-gl-joint to hinge

--- kinematics/open-gl-hinge/open-gl-hinge.cpp
+++ kinematics/open-gl-hinge/open-gl-hinge.cpp
@@ -1,14 +1,14 @@
-#define GLFW_INCLUDE_GLU
 #include "../../GLFW/glfw3.h"
+#include "../../glm/glm.hpp"
 
 #include "../../geomatric-primitives/sphere.hpp"
 
-#include "open-gl-joint.hpp"
+#include "open-gl-hinge.hpp"
 
-void OpenGLJoint::draw() {
+void OpenGLHinge::draw() {
     glPushMatrix();
     glTranslatef(related_position.x, related_position.y, related_position.z);
-    glRotatef(theta, axis.x, axis.y, axis.z);
+    glRotatef(angle, axis.x, axis.y, axis.z);
 
     glColor3f(1, 0, 0);
     OpenGLSphere sphere = OpenGLSphere(glm::vec3(0, 0, 0), 0.5f);
---
+++ kinematics/open-gl-hinge/open-gl-hinge.hpp
@@ -0,0 +1,11 @@
+#pragma once
+
+#include "../hinge/hinge.hpp"
+
+class OpenGLHinge : public Hinge {
+public:
+    OpenGLHinge() {
+    }
+
+    virtual void draw();
+};
