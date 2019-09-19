181afba06e2dfec6cc045e90d3abb47478df19c4
Leeingnyo <dyd1928@naver.com>
2019-09-19 17:15:17 +0900

    fix open-gl-link

--- kinematics/open-gl-link/open-gl-link.cpp
+++ kinematics/open-gl-link/open-gl-link.cpp
@@ -9,8 +9,10 @@ void OpenGLLink::draw() {
     glPushMatrix();
 
     glColor3f(1, 1, 1);
-    OpenGLCylinder cylinder = OpenGLCylinder(glm::vec3(0, 0.5f, 0), glm::vec3(0, length - 0.5f, 0), 0.5f);
-    cylinder.draw();
+    if (length > 0.5f) {
+        OpenGLCylinder cylinder = OpenGLCylinder(glm::vec3(0, 0.5f, 0), glm::vec3(0, length - (joints.size() ? 0.5f : 0.f), 0), 0.5f);
+        cylinder.draw();
+    }
 
     glTranslatef(0, length, 0);
     for (auto joint : joints) {
