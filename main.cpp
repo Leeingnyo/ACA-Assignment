295fa934f6efc78af0a20e17a629c2ad9d1d743c
Leeingnyo <dyd1928@naver.com>
2019-09-29 20:47:27 +0900

    add an example instead of human
    근데 버그 있음...

--- main.cpp
+++ main.cpp
@@ -25,7 +25,11 @@
 #include "kinematics/open-gl-hinge/open-gl-hinge.hpp"
 #include "kinematics/open-gl-ball-and-socket/open-gl-ball-and-socket.hpp"
 #include "kinematics/open-gl-link/open-gl-link.hpp"
+#include "kinematics/open-gl-euler-joint/open-gl-euler-joint.hpp"
+#include "kinematics/root-joint/root-joint.hpp"
 #include "kinematics/human/human.hpp"
+
+#include "kinematics/bvh-to-kinematics/bvh-to-kinematics.hpp"
 
 #ifndef __WIN32
 #define __int64 long long
@@ -66,7 +70,11 @@ int main () {
     // glEnable(GL_LIGHT0);
     // glEnable(GL_COLOR_MATERIAL);
 
-    OpenGLHuman human = OpenGLHuman();
+    const std::string example = ("HIERARCHY\nROOT pelvis\n{\n	OFFSET 0.000000 0.000000 0.000000\n	CHANNELS 6 Xposition Yposition Zposition Zrotation Xrotation Yrotation\n	JOINT lfemur\n	{\n		OFFSET 11.446000 0.000000 0.000000\n		CHANNELS 3 Zrotation Xrotation Yrotation\n		JOINT ltibia\n		{\n			OFFSET 0.000000 -42.030400 0.000000\n			CHANNELS 3 Zrotation Xrotation Yrotation\n			JOINT lfoot\n			{\n				OFFSET 0.000000 -39.930200 0.000000\n				CHANNELS 3 Zrotation Xrotation Yrotation\n				JOINT ltoes\n				{\n					OFFSET 0.000000 0.000000 11.775800\n					CHANNELS 3 Zrotation Xrotation Yrotation\n					End Site\n					{\n					    OFFSET 0.000000 0.000000 5.000000\n					}\n				}\n			}\n		}\n	}\n	JOINT rfemur\n	{\n		OFFSET -11.446000 0.000000 0.000000\n		CHANNELS 3 Zrotation Xrotation Yrotation\n		JOINT rtibia\n		{\n			OFFSET 0.000000 -42.030400 0.000000\n			CHANNELS 3 Zrotation Xrotation Yrotation\n			JOINT rfoot\n			{\n				OFFSET 0.000000 -39.930200 0.000000\n				CHANNELS 3 Zrotation Xrotation Yrotation\n				JOINT rtoes\n				{\n					OFFSET 0.000000 0.000000 11.775800\n					CHANNELS 3 Zrotation Xrotation Yrotation\n					End Site\n					{\n					    OFFSET 0.000000 0.000000 5.000000\n					}\n				}\n			}\n		}\n	}\n	JOINT thorax\n	{\n		OFFSET 0.000000 11.255600 -5.000000\n		CHANNELS 3 Zrotation Xrotation Yrotation\n		JOINT head\n		{\n			OFFSET 0.000000 43.799300 0.000000\n			CHANNELS 3 Zrotation Xrotation Yrotation\n			End Site\n			{\n			    OFFSET 0.000000 5.000000 0.000000\n			}\n		}\n		JOINT lclavicle\n		{\n			OFFSET 0.000000 39.863000 0.000000\n			CHANNELS 3 Zrotation Xrotation Yrotation\n			JOINT lhumerus\n			{\n				OFFSET 17.380600 4.958300 0.000000\n				CHANNELS 3 Zrotation Xrotation Yrotation\n				JOINT lradius\n				{\n					OFFSET 0.000000 -27.561300 0.000000\n					CHANNELS 3 Zrotation Xrotation Yrotation\n					JOINT lhand\n					{\n						OFFSET 0.000000 -26.933000 0.000000\n						CHANNELS 3 Zrotation Xrotation Yrotation\n						End Site\n						{\n						    OFFSET 0.000000 -5.000000 0.000000\n						}\n					}\n				}\n			}\n		}\n		JOINT rclavicle\n		{\n			OFFSET 0.000000 39.863000 0.000000\n			CHANNELS 3 Zrotation Xrotation Yrotation\n			JOINT rhumerus\n			{\n				OFFSET -17.380600 4.958300 0.000000\n				CHANNELS 3 Zrotation Xrotation Yrotation\n				JOINT rradius\n				{\n					OFFSET 0.000000 -27.561300 0.000000\n					CHANNELS 3 Zrotation Xrotation Yrotation\n					JOINT rhand\n					{\n						OFFSET 0.000000 -26.933000 0.000000\n						CHANNELS 3 Zrotation Xrotation Yrotation\n						End Site\n						{\n						    OFFSET 0.000000 -5.000000 0.000000\n						}\n					}\n				}\n			}\n		}\n	}\n}\nMOTION\nFrames: 3\nFrame Time: 0.008342\n59.293900 87.796900 4.760200 -0.467276 -1.378122 -89.563821 -5.641307 2.819435 -8.651036 -0.000000 9.240240 -0.000000 0.000000 3.555270 6.177856 0.000000 -1.301336 -0.000000 -1.803037 2.742629 -3.741876 -0.000000 9.274983 -0.000000 0.000000 3.290364 2.906258 0.000000 -29.528157 0.000000 -4.531867 0.132349 0.213401 15.628959 24.623904 -0.608822 0.394562 0.000000 3.487028 83.769402 0.553653 62.001030 -0.000001 -12.781487 -55.037055 5.615823 0.703335 0.000000 0.095169 -0.000000 -0.923419 -78.312883 9.001808 -58.564139 0.000000 -20.003803 36.764996 -3.936911 -2.063559 -0.000000 \n59.291700 87.793600 4.752400 -0.475906 -1.381233 -89.556452 -5.638376 2.830551 -8.625235 0.000000 9.236131 -0.000000 -0.000000 3.523541 6.156061 0.000000 -1.301682 -0.000000 -1.792531 2.748974 -3.691834 -0.000000 9.276495 0.000000 0.000000 3.262773 2.860635 0.000000 -29.527798 0.000000 -4.547519 0.143945 0.207558 15.646355 24.636287 -0.589535 0.469632 -0.000000 3.531537 83.641881 0.487220 62.085857 -0.000000 -12.806394 -55.155542 5.630933 0.721572 0.000000 0.133438 -0.000000 -0.934718 -78.356085 8.980916 -58.621133 0.000000 -19.969767 36.707936 -3.798525 -2.110204 -0.000000");
+    inyong_bvh::BvhParser parser = inyong_bvh::BvhParser();
+    auto bvh_tokens = parser.scan_from_string(example);
+    auto bvh = parser.parse_from_tokens(bvh_tokens);
+    auto root = bvh_to_kinematics(bvh);
 
     auto starttime = std::chrono::system_clock::now();
 
@@ -82,46 +90,7 @@ int main () {
             unsigned __int64 delta_micro = std::chrono::duration_cast<std::chrono::microseconds>(current - starttime).count();
             unsigned __int64 delta_milli = std::chrono::duration_cast<std::chrono::milliseconds>(current - starttime).count();
 
-            const float shoulder_angle = 30.f;
-            const float elbow_angle = 120.f;
-            human.right_shoulder_joint->direction = glm::vec3(-std::cos(shoulder_angle * M_PI / 180), 0, std::sin(shoulder_angle * M_PI / 180));
-            human.right_shoulder_joint->front = glm::vec3(0, 0, 1);
-            human.right_elbow_joint->angle = elbow_angle;
-            human.left_shoulder_joint->direction = glm::vec3(std::cos(shoulder_angle * M_PI / 180), 0, std::sin(shoulder_angle * M_PI / 180));
-            human.left_shoulder_joint->front = glm::vec3(0, 0, 1);
-            human.left_elbow_joint->angle = elbow_angle;
-
-            const float one_cycle_per_second = delta_milli * 360 / 1000.f;
-            const float one_radian_per_second = one_cycle_per_second * M_PI / 180.f;
-            const float& x = one_radian_per_second;
-            const float sin_x = std::sin(x);
-            const float cos_x = std::cos(x);
-            const float cycle = std::sin(x / 2);
-            const float cycle_half = std::sin(x / 4);
-            human.backbone_joint->front = glm::vec3(std::sin(cycle * 120 * M_PI / 180),
-                    0, std::cos(cycle * 120 * M_PI / 180));
-            human.backbone_joint->direction  = glm::vec3(std::sin(cycle * 10 * M_PI / 180),
-                    std::cos(cycle * 10 * M_PI / 180), 0);
-
-            human.right_hip_joint->direction = glm::vec3(
-                    -std::sin(-std::max(-cycle * 30, 0.f) * M_PI / 180),
-                    -std::cos(std::max(-cycle * 120, 0.f) * M_PI / 180),
-                    std::sin(std::max(-cycle * 120, 0.f) * M_PI / 180));
-            human.right_hip_joint->front = glm::vec3(
-                    std::sin(std::max(-cycle * 30, 0.f) * M_PI / 180),
-                    std::sin(std::max(-cycle * 90, 0.f) * M_PI / 180),
-                    std::cos(std::max(-cycle * 90, 0.f) * M_PI / 180));
-            human.right_knee_joint->angle = std::max(-cycle * 150, 0.f);
-
-            human.left_hip_joint->direction = glm::vec3(
-                    std::sin(-std::max(cycle * 30, 0.f) * M_PI / 180),
-                    -std::cos(std::max(cycle * 120, 0.f) * M_PI / 180),
-                    std::sin(std::max(cycle * 120, 0.f) * M_PI / 180));
-            human.left_hip_joint->front = glm::vec3(
-                    -std::sin(std::max(cycle * 30, 0.f) * M_PI / 180),
-                    std::sin(std::max(cycle * 90, 0.f) * M_PI / 180),
-                    std::cos(std::max(cycle * 90, 0.f) * M_PI / 180));
-            human.left_knee_joint->angle = std::max(cycle * 150, 0.f);
+            root->animate((int)(delta_milli / 1000) % 2);
         }
 
         { // projection
@@ -149,7 +118,7 @@ int main () {
             glLightfv(GL_LIGHT0, GL_POSITION, position);
             */
 
-            human.draw();
+            root->draw();
 
             glBegin(GL_LINES);
             glColor3f(1, 0, 0);
